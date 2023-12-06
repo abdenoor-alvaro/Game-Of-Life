00d55191c82549521c1f46dcf65718253fccd140
Your Name <medjerneneabdenoor@gmail.com>
2023-12-06 21:54:19 +0100

    improving the sours code with the help of chat gpt

--- main.js
+++ main.js
@@ -12,68 +12,94 @@ class Player {
         this.Points += roundPoints
     }
 }
-let mahfoud = new Player("mahfoud", "mahfoud.jpg")
-let islem = new Player("islem", "islem.jpg")
-let abdenoor = new Player("abdenoor", "slimani.jpg")
-let alvaro = new Player("alvaro", "abdenoor.jpg")
-let yacine = new Player("yacine", "yacine.jpg")
-let youcef = new Player("youcef", "youcef.jpg")
-let players = [mahfoud, islem, abdenoor, alvaro, yacine, youcef]
 
-let days = [
+const playersData = [
+    {name:"boussebain mahfoud",image: "mahfoud.jpg"},
+    {name:"bourmel islem",image: "islem.jpg"},
+    {name:"slimani abdenoor",image: "slimani.jpg"},
+    {name:"abdenoor alvaro",image: "abdenoor.jpg"},
+    {name:"sahel yacine",image: "yacine.jpg"},
+    {name:"salem youcef",image: "youcef.jpg"},
+]
+
+const daysData = [
     {
         day: 1,
-        alvaro: 48,
-        yacine: 58.5,
-        islem: 47,
-        mahfoud: 66,
-        abdenoor: 45,
-        youcef: 33,
-    },
-    {
-        day:2,
-        alvaro: 39,
-        yacine: 36,
-        islem: 45.5,
-        mahfoud: 52,
-        abdenoor: 37,
-        youcef: 17,
-    },
-    {
-        day:3,
-        alvaro: false,
-        yacine: false,
-        islem: false,
-        mahfoud: false,
-        abdenoor: false,
-        youcef: false,
-    },
-    {
-        day:4,
-        alvaro: false,
-        yacine: false,
-        islem: false,
-        mahfoud: false,
-        abdenoor: false,
-        youcef: false,
-    },
-    {
-        day:5,
-        alvaro: false,
-        yacine: false,
-        islem: false,
-        mahfoud: false,
-        abdenoor: false,
-        youcef: false,
+        scores: {
+            abdenoor_alvaro: 48,
+            sahel_yacine: 58.5,
+            bourmel_islem: 47,
+            boussebain_mahfoud: 66,
+            slimani_abdenoor: 45,
+            salem_youcef: 33,
+        }
+    },{
+        day: 2,
+        scores: {
+            abdenoor_alvaro: 39,
+            sahel_yacine: 36,
+            bourmel_islem: 45.5,
+            boussebain_mahfoud: 52,
+            slimani_abdenoor: 37,
+            salem_youcef: 17,
+        } 
+    },{
+        day: 3,
+        scores: {
+            abdenoor_alvaro: 49,
+            sahel_yacine: false,
+            bourmel_islem: false,
+            boussebain_mahfoud: 72,
+            slimani_abdenoor: false,
+            salem_youcef: false,
+        }
+    },{
+        day: 4,
+        scores: {
+            abdenoor_alvaro: false,
+            sahel_yacine: false,
+            bourmel_islem: false,
+            boussebain_mahfoud: false,
+            slimani_abdenoor: false,
+            salem_youcef: false,
+        }
+    },{
+        day: 5,
+        scores: {
+            abdenoor_alvaro: false,
+            sahel_yacine: false,
+            bourmel_islem: false,
+            boussebain_mahfoud: false,
+            slimani_abdenoor: false,
+            salem_youcef: false,
+        }
     },
 ]
 // End Data
-
-
-
 // Start Functions
-function tableHtml(location, players) {
-    let html = `
+function calculatePoints(players, daysData) {
+    for (const player of players) {
+        let playerName = player.Name
+        if (player.Name.split(" ").length > 1) {
+            playerName = player.Name.split(" ").join("_")
+        }
+        for (const day of daysData) {
+            const scores = day.scores
+            if (scores[playerName]) {
+                player.newRound(scores[playerName])
+            }
+        }
+    }
+}
+
+
+function capitalize(sentence) {
+    return sentence.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+}
+
+function generateHtmlTable(players) {
+    const htmlTableLocation = document.querySelector(".table")
+    const html = `
     <div class="headline w-100 d-flex justify-content-between">
         <span class="rank">Rank</span>
         <span class="image"></span>
@@ -81,80 +107,32 @@ function tableHtml(location, players) {
         <span class="round">Round</span>
         <span class="points">Points</span>
     </div>
-    ${players}
+    ${players.map(player => generatePlayerHtml(player)).join("")}
     `
-    location.innerHTML = html
+    htmlTableLocation.innerHTML = html
 }
-function playerHtml(...list) {
-    let HTML = `
+function generatePlayerHtml(player) {
+    return `
     <div class="playerline w-100 d-flex justify-content-between align-items-center bg-white py-3">
-        <span class="rank fw-bold">${list[3]}</span>
-        <span class="image"><img src="images/${list[4]}" alt=""></span>
-        <span class="player flex-grow-1">${capitalize(list[1])}</span>
-        <span class="round">${list[5]}</span>
-        <span class="points">${list[2]}</span>
+        <span class="rank fw-bold">${player.Rank}</span>
+        <span class="image"><img src="images/${player.Image}" alt=""></span>
+        <span class="player flex-grow-1">${capitalize(player.Name)}</span>
+        <span class="round">${player.Round}</span>
+        <span class="points">${player.Points}</span>
     </div>
     `
-    list[0].innerHTML += HTML
-    return list[0].innerHTML
-}
-function capitalize(sentence) {
-    let teamName = sentence.split(" ")
-    for (let j = 0; j < teamName.length; j++) {
-        teamName[j] = `${teamName[j].charAt(0).toUpperCase()}${teamName[j].slice(1)}`
-    }
-    return  teamName.join(" ")
 }
 // End Functions
-
-
-
 // Start Calculating Points
-for (let i = 0; i < players.length; i++) {
-    let playerName = players[i].Name
-    for (let n = 0; n < days.length; n++) {
-        let keys = Object.keys(days[n])
-        let values = Object.values(days[n])
-        for (let x = 0; x < keys.length; x++) {
-            if (playerName === keys[x] && values[x] !== false) {
-                players[i].newRound(values[x])
-            }
-        }
-    }
-}
+const players = playersData.map(playerData => new Player(playerData.name, playerData.image))
+calculatePoints(players,daysData)
 // End Calculating Points
-
-
 // Start Sorting and Printing
-let htmlTableLocation = document.querySelector(".table")
-let div = document.createElement("div")
-let sortedArray = []
-for (let i = 0; i < players.length; i++) { 
-    let bestValue = -1
-    let nameOfBestValue = ""
-    for (let i = 0; i < players.length; i++){
-        if (!sortedArray.includes(players[i].Name)) {
-            if (players[i].Points > bestValue) {
-                bestValue = players[i].Points
-                nameOfBestValue = players[i].Name
-            }
-        }
-    }
-
-    let rank = i +1
-    for (let x = 0; x < players.length; x++) {
-        if (nameOfBestValue === players[x].Name) {
-            if (i === players.length - 1) {
-                tableHtml(htmlTableLocation, playerHtml(div, players[x].Name, players[x].Points, rank, players[x].Image, players[x].Round))
-            } else {
-                playerHtml(div, players[x].Name, players[x].Points, rank, players[x].Image, players[x].Round)
-            }
-            break 
-        }
-    }
-
-    sortedArray.push(nameOfBestValue)
-}
+const sortedPlayers = [...players].sort((a, b) => b.Points - a.Points)
+sortedPlayers.forEach((player, index) => {
+    player.Rank = index +1
+})
+generateHtmlTable(sortedPlayers)
 // End Sorting and Printing
 
 
