ce447904f257c369f1e833baa64061ba9f823991
Your Name <medjerneneabdenoor@gmail.com>
2023-12-09 18:04:22 +0100

    update latest points

--- main.js
+++ main.js
@@ -82,10 +82,10 @@ const daysData = [
         date: "Thursday 07 December 2023",
         scores: {
             abdenoor_alvaro: 44.5,
-            sahel_yacine: false,
+            sahel_yacine: 59,
             bourmel_islem: 50.5,
             boussebain_mahfoud: 73.5,
-            slimani_abdenoor: false,
+            slimani_abdenoor: 49,
             salem_youcef: false,
         }
     },{
@@ -93,10 +93,10 @@ const daysData = [
         date: "Friday 08 December 2023",
         scores: {
             abdenoor_alvaro: 55,
-            sahel_yacine: false,
-            bourmel_islem: false,
-            boussebain_mahfoud: false,
-            slimani_abdenoor: false,
+            sahel_yacine: 67,
+            bourmel_islem: 34,
+            boussebain_mahfoud: 66,
+            slimani_abdenoor: 27,
             salem_youcef: false,
         }
     },
