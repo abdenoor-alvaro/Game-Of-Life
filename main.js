be8885848d5ce3ceed9872e8de88a989a00cf924
medjernene abdenoor <medjerneneabdenoor@gmail.com>
2024-01-25 07:39:22 +0100

    test

--- main.js
+++ main.js
@@ -597,6 +597,7 @@ const daysData = [
         date: "Tuesday 23 January 2024",
         scores: {
             abdenoor_alvaro: 75.5,
+            abdenoor_alvaro: 0,
             sahel_yacine: 68,
             bourmel_islem: 26.5,
             boussebain_mahfoud: 61,
